dee59b134b092a07a99b4941ddfebbbc28b79f95
Sufiyan Siddique <sufio@memyself.in>
2018-12-30 11:59:36 +0530

    uncommited codes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,13 @@ class ArticlesController < ApplicationController
 	def new
 		
 	end
+
+
+	def create
+	  @article = Article.new(params[:article])
+	 
+	  @article.save
+	  redirect_to @article
+	end
 end
+		
